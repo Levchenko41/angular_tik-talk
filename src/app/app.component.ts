dc2c87c86f6901e25e394913e5e42a1ef24593d1
Vasya Levchenko <lev4encko.vasia@gmail.com>
2025-04-08 17:08:13 +0300

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { JsonPipe } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interface';
+import { ImgUrlPipe } from './helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tik-talk';
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val: Profile[]) => {
+      this.profiles = val;
+    });
+  }
 }
